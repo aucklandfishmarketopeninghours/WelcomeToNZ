98271f1398b51763d35fdcec39532fa9e9f65755
aucklandfishmarketopeninghours <aucklandfishmarketopeninghours@gmail.com>
2017-07-30 08:51:14 +1200

    Added

---
+++ ASPApi/Models/MonthlyVisitors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPApi.Models
+{
+	public class MonthlyVisitors
+	{
+		[Key]
+		public string Month { get; set; }
+		public string Year5 { get; set; }
+	}
+}
